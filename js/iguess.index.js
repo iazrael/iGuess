daa78a52eec67baddf6c5d85e7cde22a65750143
azrael <azrael@imatlas.com>
2012-06-09 17:57:45 +0800

    up

--- js/iguess.index.js
+++ js/iguess.index.js
@@ -3,6 +3,7 @@ Z.$package('iGuess.index', function(z){
     // 整个应用的入口
     this.init = function(){
         iGuess.socket.connect();
-        iGuess.socket.send({type: 'getuid'});
-    }   
+    }
+
+
 });
