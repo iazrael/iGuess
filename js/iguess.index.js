fd23818dc39da70e09deded848d0495ad3a5ed57
azrael <azrael@imatlas.com>
2012-06-10 14:42:44 +0800

    up

--- js/iguess.index.js
+++ js/iguess.index.js
@@ -1,18 +1,30 @@
 Z.$package('iGuess.index', function(z){
 
-
+    var $body;
 
 	// 整个应用的入口
     this.init = function(){
-    	
+    	$body = $('.body');
+
         iGuess.socket.connect();
  
-	  	
+	  	iGuess.wait.init();
+        iGuess.ask.init();
+        iGuess.main.init();
 	  	iGuess.socket.on('connect',function(){
 	  		iGuess.stargModuler.init();
 	  	});
-        iGuess.wait.init();
-        iGuess.ask.init();
-        iGuess.main.init();
-    }   
+        
+        // $(window).resize(onResize);
+        // onResize();
+    }
+
+    var onResize = function(){
+        var size = {
+            width: window.innerWidth,
+            height: window.innerHeight - 40 - 20
+        };
+        $body.height(size.height);
+        z.message.notify('resize', size);
+    };
 });
